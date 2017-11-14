0550f10861478f1bec02beb27b597d664041a77d
Huang Peng <huangpeng@chunyu.me>
2017-11-14 12:58:46 +0800

    MOD: add object convert from stream

--- http/src/main/java/me/chunyu/http/okhttp/response/JSON.kt
+++ http/src/main/java/me/chunyu/http/okhttp/response/JSON.kt
@@ -2,6 +2,8 @@ package me.chunyu.http.okhttp.response
 
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import com.google.gson.stream.JsonReader
+import java.io.Reader
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
@@ -17,6 +19,12 @@ class JSON {
             val gson = gsonBuilder.create()
 
             return gson.fromJson<T>(jsonString, type)
+        }
+
+        fun<T> toObject(reader: Reader, type: Type): T {
+            val gson = gsonBuilder.create()
+
+            return gson.fromJson(reader, type)
         }
 
         fun<T> toJSONString(d: T): String {
